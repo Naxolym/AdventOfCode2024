1092f4a039a62f7840cb48fc7f1436922c429d61
Naxolym <naxolym@gmail.com>
2024-12-03 17:59:52 +0100

    add updating of daily solution index table

--- update_readme.py
+++ update_readme.py
@@ -1,21 +1,72 @@
+import json
 import re
+from datetime import datetime
 from leaderboard_data import JSONLeaderboard, AoCUser
 
+PUZZLE_DATA_FILENAME = 'puzzle_data.json'
+README_TEMPLATE_FILENAME = 'README.md.template'
 README_FILENAME = 'README.md'
 DAYS_COMPLETED_REGEX = re.compile(r'(Days%20Completed-)([0-9]+)(-green)')
 STAR_REGEX = re.compile(r'(Stars%20Obtained%20⭐-)([0-9]+)(-yellow)')
 NOT_SOLVED_REGEX = re.compile(r'(Not%20Solved%20❌-)([0-9]+)(-red)')
+SOLUTION_TABLE_REGEX = re.compile(r'\{\{SOLUTION_TABLE}}')
+
+TABLE_HEADER = ("| Day "
+                "|            Title            "
+                "| Part 1 "
+                "| Part 2 "
+                "|                             Code                              |"
+                "\n"
+                "|:---:"
+                "|:---------------------------:"
+                "|:------:"
+                "|:------:"
+                "|:-------------------------------------------------------------:|"
+                "\n"
+                )
+
+TABLE_ROW = ("| {{day_left_padded}}  "
+             "| [{{puzzle_name}}][day{{day_left_padded}}] "
+             "|   ⭐    "
+             "|   ⭐    "
+             "| [![Part 1][part1]](aoc-{{day}}-1.py) [![Part 2][part2]](aoc-{{day}}-2.py) |"
+             "\n"
+             )
+
+
+def build_row(day: int, name: str) -> str:
+    day_no_pad = f'{day}'
+    day_left_padded = f'{day:02}'
+
+    row = TABLE_ROW
+    row = re.sub(r'\{\{day_left_padded}}', day_left_padded, row)
+    row = re.sub(r'\{\{day}}', day_no_pad, row)
+    row = re.sub(r'\{\{puzzle_name}}', name, row)
+    return row
+
+
+def build_table() -> str:
+    table = ''
+    table += TABLE_HEADER
+    day = datetime.utcnow().day
+    with open(PUZZLE_DATA_FILENAME, "r") as f:
+        puzzle_data = json.load(f)
+    for d in range(1, day + 1):
+        table += build_row(d, puzzle_data[str(d)])
+    return table
 
 
 def update_readme(user: AoCUser):
-    with open(README_FILENAME, "r", encoding="utf-8") as f:
-        readme = f.read()
+    with open(README_TEMPLATE_FILENAME, "r", encoding="utf-8") as f:
+        template = f.read()
     f.close()
-    readme = re.sub(DAYS_COMPLETED_REGEX, rf'\g<1>{user.days_completed}\g<3>', readme, re.MULTILINE)
-    readme = re.sub(STAR_REGEX, rf'\g<1>{user.stars}\g<3>', readme, re.MULTILINE)
-    readme = re.sub(NOT_SOLVED_REGEX, rf'\g<1>{user.challenges_not_solved}\g<3>', readme, re.MULTILINE)
+    template = DAYS_COMPLETED_REGEX.sub(rf'\g<1>{user.days_completed}\g<3>', template, re.MULTILINE)
+    template = STAR_REGEX.sub(rf'\g<1>{user.stars}\g<3>', template, re.MULTILINE)
+    template = NOT_SOLVED_REGEX.sub(rf'\g<1>{user.challenges_not_solved}\g<3>', template, re.MULTILINE)
+    solution_table = build_table()
+    template = SOLUTION_TABLE_REGEX.sub(solution_table, template)
     with open(README_FILENAME, "w", encoding="utf-8") as f:
-        f.write(readme)
+        f.write(template)
     f.close()
 
 
